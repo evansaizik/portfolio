e0b4944f687c0acd171b5a30e638f02c8e27eef7
isaiah usiholo <evansaizik@gmail.com>
2022-11-19 23:10:05 +0100

    about component

---
+++ src/components/about/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from './About.module.css';
+
+const About = () => {
+  return  <>
+    <h1 className={classes.name}>Evans</h1>
+    <span>6 Akanbi Ogundare street, unity estate, iba, ojo</span>
+    <span>+234(0)8138836470</span>
+    <span>evansaizik@gmail.com</span>
+  </>
+};
+
+export default About;
