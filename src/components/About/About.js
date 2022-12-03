aaced62e8166a6aa462da9058576559bce5f1a59
isaiah usiholo <evansaizik@gmail.com>
2022-12-03 08:07:31 +0100

    typo

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,7 @@ const About = () => {
     <span>6 Akanbi Ogundare street, unity estate, iba, ojo, Lagos. </span>
     <span>+234(0)8138836470. </span>
     <span className={classes.email}>evansaizik@gmail.com</span>
-    <p>I am experienced in leveraging agile frameworks to provide a robust frontend implementations for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition</p>
+    <p>I am experienced in leveraging agile frameworks to provide robust frontend implementations for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition</p>
 
     <section className={classes.social}>
       <img src={github} alt="github link" />
